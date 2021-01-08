95360d491b199fcdad4e4c449c2500d19c4d792f
Travis Ralston <travpc@gmail.com>
2021-01-08 10:26:39 -0700

    Prove that imports work

--- web/devroom.ts
+++ web/devroom.ts
@@ -1,3 +1,9 @@
+import { MatrixCapabilities, WidgetApi } from "matrix-widget-api";
+
 const el = document.createElement("div");
 el.innerText = "Hello world";
 document.body.appendChild(el);
+
+// Example widget (to prove imports work)
+const testWidget = new WidgetApi(/*widgetId*/);
+testWidget.requestCapability(MatrixCapabilities.AlwaysOnScreen);
