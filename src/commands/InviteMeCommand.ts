f907d1fa4c997ec0003e2122da343642890aff51
Travis Ralston <travpc@gmail.com>
2021-02-05 03:52:08 -0700

    Add a public invite command

--- src/commands/InviteMeCommand.ts
+++ src/commands/InviteMeCommand.ts
@@ -15,11 +15,12 @@ limitations under the License.
 */
 
 import { ICommand } from "./ICommand";
-import { MatrixClient } from "matrix-bot-sdk";
+import { LogLevel, MatrixClient } from "matrix-bot-sdk";
 import { Conference } from "../Conference";
 import { LiveWidget } from "../models/LiveWidget";
 import { invitePersonToRoom, ResolvedPersonIdentifier } from "../invites";
 import { Role } from "../db/DbPerson";
+import { logMessage } from "../LogProxy";
 
 export class InviteMeCommand implements ICommand {
     public readonly prefixes = ["inviteme", "inviteto"];
@@ -28,9 +29,30 @@ export class InviteMeCommand implements ICommand {
         if (!args.length) {
             return client.replyNotice(roomId, event, "Please specify a room ID or alias");
         }
-        const targetRoomId = await client.resolveRoom(args[0]);
         const userId = args[1] || event['sender'];
-        await client.inviteUser(userId, targetRoomId);
+        if (args[0] === "public") {
+            for (const aud of conference.storedAuditoriums) {
+                try {
+                    const members = await client.getJoinedRoomMembers(aud.roomId);
+                    if (members.includes(userId)) continue;
+                    await client.inviteUser(userId, aud.roomId);
+                } catch (e) {
+                    await logMessage(LogLevel.WARN, "InviteMeCommand", `Error inviting ${userId} to ${aud.roomId}: ${e?.message || e?.body?.message}`);
+                }
+            }
+            for (const spi of conference.storedInterestRooms) {
+                try {
+                    const members = await client.getJoinedRoomMembers(spi.roomId);
+                    if (members.includes(userId)) continue;
+                    await client.inviteUser(userId, spi.roomId);
+                } catch (e) {
+                    await logMessage(LogLevel.WARN, "InviteMeCommand", `Error inviting ${userId} to ${spi.roomId}: ${e?.message || e?.body?.message}`);
+                }
+            }
+        } else {
+            const targetRoomId = await client.resolveRoom(args[0]);
+            await client.inviteUser(userId, targetRoomId);
+        }
         await client.replyNotice(roomId, event, "Invite sent");
     }
 }
