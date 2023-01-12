b3a169505e63aaf79a449e7fa8ad18615c01a26f
Olivier Wilkinson (reivilibre) <oliverw@matrix.org>
2023-01-12 16:28:54 +0000

    Extract `inviteTo`

--- src/commands/InviteMeCommand.ts
+++ src/commands/InviteMeCommand.ts
@@ -18,32 +18,34 @@ import { ICommand } from "./ICommand";
 import { LogLevel, MatrixClient } from "matrix-bot-sdk";
 import { Conference } from "../Conference";
 import { LiveWidget } from "../models/LiveWidget";
-import { invitePersonToRoom, ResolvedPersonIdentifier } from "../invites";
 import { logMessage } from "../LogProxy";
 
 export class InviteMeCommand implements ICommand {
     public readonly prefixes = ["inviteme", "inviteto"];
+
+    private async inviteTo(client: MatrixClient, invitee: string, room: string): Promise<void> {
+        const members = await client.getJoinedRoomMembers(room);
+        if (members.includes(invitee)) return;
+        await client.inviteUser(invitee, room);
+    }
 
     public async run(conference: Conference, client: MatrixClient, roomId: string, event: any, args: string[]) {
         if (!args.length) {
             return client.replyNotice(roomId, event, "Please specify a room ID or alias");
         }
         const userId = args[1] || event['sender'];
+
         if (args[0] === "public") {
             for (const aud of conference.storedAuditoriums) {
                 try {
-                    const members = await client.getJoinedRoomMembers(aud.roomId);
-                    if (members.includes(userId)) continue;
-                    await client.inviteUser(userId, aud.roomId);
+                    await this.inviteTo(client, userId, aud.roomId);
                 } catch (e) {
                     await logMessage(LogLevel.WARN, "InviteMeCommand", `Error inviting ${userId} to ${aud.roomId}: ${e?.message || e?.body?.message}`);
                 }
             }
             for (const spi of conference.storedInterestRooms) {
                 try {
-                    const members = await client.getJoinedRoomMembers(spi.roomId);
-                    if (members.includes(userId)) continue;
-                    await client.inviteUser(userId, spi.roomId);
+                    await this.inviteTo(client, userId, spi.roomId);
                 } catch (e) {
                     await logMessage(LogLevel.WARN, "InviteMeCommand", `Error inviting ${userId} to ${spi.roomId}: ${e?.message || e?.body?.message}`);
                 }
