78c1e3daaa680ff9776dc3f771f287dbd2a165fd
Half-Shot <will@half-shot.uk>
2023-02-04 09:32:19 +0000

    Increase wait time for plumb command

--- src/commands/IrcPlumbCommand.ts
+++ src/commands/IrcPlumbCommand.ts
@@ -21,7 +21,7 @@ import { IRCBridge } from "../IRCBridge";
 import { logMessage } from "../LogProxy";
 import { KickPowerLevel } from "../models/room_kinds";
 
-const PLUMB_WAIT_MS = 1000;
+const PLUMB_WAIT_MS = 10000;
 
 export class IrcPlumbCommand implements ICommand {
     public readonly prefixes = ["plumb-irc"];
