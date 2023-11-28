4cf462a38386cb32507e8020aa5fdb451cca2730
H. Shay <hillerys@element.io>
2023-11-27 19:36:50 -0800

    yard add querystring 3?

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,7 @@ module.exports = {
     },
     resolve: {
         extensions: ['.ts', '.js'],
+        fallback: { "querystring": require.resolve("querystring-es3") }
     },
     plugins: [
         new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
