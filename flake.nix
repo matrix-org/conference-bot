0b54630d428281d8c6b63cdd5309deebf78e08bb
reivilibre <oliverw@matrix.org>
2024-01-09 16:34:04 +0000

    Add a Nix flake for development (#199)

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Conference Bot";
+
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-23.05";
+    devenv.url = "github:cachix/devenv/v0.6.3";
+  };
+
+  outputs = inputs @ { self, nixpkgs, utils, devenv }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages."${system}";
+    in rec {
+      # `nix develop`
+      devShell = devenv.lib.mkShell {
+        inherit inputs pkgs;
+        modules = [
+          {
+            languages = {
+              javascript.enable = true;
+              typescript.enable = true;
+            };
+
+            env = {
+              # Work around 'digital envelope routines::unsupported'
+              # source: https://stackoverflow.com/a/75536711
+              NODE_OPTIONS = "--openssl-legacy-provider";
+            };
+
+            packages = with pkgs; [
+              yarn
+            ];
+          }
+        ];
+      };
+    });
+}
