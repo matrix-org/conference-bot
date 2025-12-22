fa338379d9f6d00326df5ba20e5df3241dbc7d5d
Olivier 'reivilibre <oliverw@matrix.org>
2025-12-22 12:29:11 +0000

    Apply suggestions from code review
    Co-authored-by: Andrew Morgan <1342360+anoadragon453@users.noreply.github.com>

--- src/backends/json/jsontypes/FosdemJsonSchedule.schema.d.ts
+++ src/backends/json/jsontypes/FosdemJsonSchedule.schema.d.ts
@@ -49,7 +49,7 @@ export interface FOSDEMTalk {
     [k: string]: unknown;
   };
   /**
-   * System name (not human-friendly) of the physical (in real life) room that the talk is held in. e.g. `ud2218a`
+   * System (not human-friendly) name of the physical (in real life) room that the talk is held in. e.g. `ud2218a`
    */
   conference_room: string;
   [k: string]: unknown;
