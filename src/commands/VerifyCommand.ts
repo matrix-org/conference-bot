ae171c484d352e76598ab35d30029f36a983682e
Olivier 'reivilibre <oliverw@matrix.org>
2025-12-24 19:10:10 +0000

    Rip out mentions of talks in commands

--- src/commands/VerifyCommand.ts
+++ src/commands/VerifyCommand.ts
@@ -17,7 +17,6 @@ limitations under the License.
 import { ICommand } from "./ICommand";
 import { MatrixClient } from "matrix-bot-sdk";
 import { Conference } from "../Conference";
-import { asyncFilter } from "../utils";
 import { Auditorium } from "../models/Auditorium";
 import { PhysicalRoom } from "../models/PhysicalRoom";
 import { InterestRoom } from "../models/InterestRoom";
@@ -85,17 +84,6 @@ export class VerifyCommand implements ICommand {
             html += "</ul><b>Backstage room:</b><ul>";
             appendPeople(audBackstageToInvite, audToMod);
             html += "</ul>";
-
-            const talks = await asyncFilter(this.conference.storedTalks, async t => t.getAuditoriumId() === room!.getId());
-            for (const talk of talks) {
-                const talkToInvite = await this.conference.getInviteTargetsForTalk(talk);
-                const talkToMod = await this.conference.getModeratorsForTalk(talk);
-                if (talkToMod.length || talkToInvite.length) {
-                    html += `<b>Talk: ${talk.getName()} (${talk.getId()})</b><ul>`;
-                    appendPeople(talkToInvite, talkToMod);
-                    html += "</ul>";
-                }
-            }
         }
 
         await this.client.sendHtmlNotice(roomId, html);
